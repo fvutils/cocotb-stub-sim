ad7baab757347fa62f47df67293808451ec9af7c
Matthew Ballance <matt.ballance@gmail.com>
2022-08-02 18:13:52 -0700

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/cocotb_stub_sim/__init__.py
+++ src/cocotb_stub_sim/__init__.py
@@ -2,6 +2,11 @@
 import sys
 import importlib
 
-sys.modules['cocotb.simulator'] = importlib.import_module("cocotb_stub_sim.stub.cocotb.simulator")
+sys.modules['cocotb.simulator'] = importlib.import_module("cocotb_stub_sim.stub.simulator")
+sys.modules['cocotb.regression'] = importlib.import_module("cocotb_stub_sim.stub.regression")
 
 from cocotb import *
+
+def run(coro):
+    pass
+
